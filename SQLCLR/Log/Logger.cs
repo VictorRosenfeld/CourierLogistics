0a6736d6d9f52a1d1f4950168fdfa4a43c065c98
VictorRosenfeld <vrr03@mail.ru>
2021-06-29 19:58:39 +0500

    Add conditional print operators in CreateDeliveries

---
+++ SQLCLR/Log/Logger.cs
@@ -0,0 +1,74 @@
+
+namespace SQLCLR.Log
+{
+    using System;
+    //using System.IO;
+
+    /// <summary>
+    /// Простой ежедневный логгер
+    /// </summary>
+    public class Logger
+    {
+        private const string LOG_FILE_PATTERN = @"C:\LogisticsService\Log\LS_CLR_{0}.log";
+
+        /// <summary>
+        /// Шаблон сообщения
+        /// {0} - дата-время
+        /// {1} - номер сообщения
+        /// {2} - тип сообщения
+        /// {3} - текст сообщения
+        /// </summary>
+        private const string MESSAGE_PATTERN = @"@{0} {1} {2} > {3}";
+
+        /// <summary>
+        /// Построить имя файла ежедневного лога
+        /// </summary>
+        /// <returns>Имя файла лога</returns>
+        private static string GetLogFileName()
+        {
+            return string.Format(LOG_FILE_PATTERN, DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+
+        /// <summary>
+        /// Запись в лог
+        /// </summary>
+        /// <param name="messageNo">Номер сообщения</param>
+        /// <param name="message">Текст сообщения</param>
+        /// <param name="severity">Тип сообщения (-1 - не печатать; 0 - info; 1 - warn; 2 - error</param>
+        public static void WriteToLog(int messageNo, string message, int severity)
+        {
+            try
+            {
+                // 1. Форматируем тип сообщения
+                string messageType = null;
+                switch (severity)
+                {
+                    case -1:
+                        messageType = "";
+                        break;
+                    case 0:
+                        messageType = "info";
+                        break;
+                    case 1:
+                        messageType = "warn";
+                        break;
+                    case 2:
+                        messageType = "error";
+                        break;
+                    default:
+                        messageType = severity.ToString();
+                        break;
+                }
+
+                // 2. Выводим в лог
+                using (StreamWriter sw = new StreamWriter(GetLogFileName(), true))
+                {
+                    sw.WriteLine(string.Format(MESSAGE_PATTERN, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), messageNo, messageType, message));
+                    sw.Close();
+                }
+            }
+            catch
+            {   }
+        }
+    }
+}
