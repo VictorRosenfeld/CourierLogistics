b4c6c0fd6f4dc26de25fe30cffc5827ea950c305
VictorRosenfeld <vrr03@mail.ru>
2022-05-11 08:46:29 +0500

    Add messages

---
+++ DeliveryBuilder/Log/Messages.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DeliveryBuilder.Log
+{
+    internal class Messages
+    {
+        internal const string MSG_001 = "---> {0} Ver. {1} Service ID = {2}"; 
+        internal const string MSG_002 = "<--- {0} Ver. {1} Service ID = {2}";
+
+        internal const string MSG_666 = "Method {0}. {1}";
+        internal const string MSG_667 = "Method {0}. rc = {1}";
+        internal const string MSG_668 = "{0}({1})";
+
+        internal const string MSG_003 = "Config = null"; 
+        internal const string MSG_004 = "Тэг functional_parameters. Параметр {0} не задан или имеет недопустимое значение"; 
+
+    }
+}
