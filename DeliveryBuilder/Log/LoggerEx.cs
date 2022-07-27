32c1f002b3be5a29ee8d9a20ece458e64afab78e
VictorRosenfeld <vrr03@mail.ru>
2022-07-27 10:02:23 +0500

    Add roll logs support

--- DeliveryBuilder/Log/LoggerEx.cs
+++ DeliveryBuilder/Log/LoggerEx.cs
@@ -29,9 +29,19 @@ namespace DeliveryBuilder.Log
         private static string logFilePattern;
 
         /// <summary>
-        /// Количество соханяемых ежедневных логов
+        /// Базовое имя файла лога (префикс названия с расшиением)
+        /// </summary>
+        private static string baseFilename;
+
+        /// <summary>
+        /// Количество сохраняемых ежедневных логов
         /// </summary>
         private static int savedDays;
+
+        /// <summary>
+        /// День последнего выведенного сообщения
+        /// </summary>
+        private static DateTime currentDate;
 
         /// <summary>
         /// Флаг: true - логгер создан; false - логгер не создан
@@ -63,11 +73,15 @@ namespace DeliveryBuilder.Log
                 if (savedDays <= 0)
                     savedDays = 7;
 
+                currentDate = DateTime.Now.Date;
+                baseFilename = Path.GetFileName(filename);
+                Logger.savedDays = savedDays;
+
+
                 // 3. Создаём логгер
                 rc = 3;
                 logFilePattern = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileNameWithoutExtension(filename) + "_{0}" + Path.GetExtension(filename));
-                Logger.savedDays = savedDays;
-
+                RemoveLogFiles(Path.GetDirectoryName(GetLogFileName()), baseFilename, savedDays);
                 // 4. Выход - Ok
                 rc = 0;
                 IsCreated = true;
@@ -122,15 +136,96 @@ namespace DeliveryBuilder.Log
                             break;
                     }
 
-                    using (StreamWriter sw = new StreamWriter(GetLogFileName(), true))
+                    string logFile = GetLogFileName();
+                    DateTime now = DateTime.Now;
+
+                    using (StreamWriter sw = new StreamWriter(logFile, true))
                     {
-                        sw.WriteLine(string.Format(MESSAGE_PATTERN, DateTime.Now.ToString("dd.MM.yy HH:mm:ss.fff"), msgNo, messageType, message));
+                        sw.WriteLine(string.Format(MESSAGE_PATTERN, now.ToString("dd.MM.yy HH:mm:ss.fff"), msgNo, messageType, message));
                         sw.Close();
+                    }
+
+                    if (now.Date != currentDate)
+                    {
+                        RemoveLogFiles(Path.GetDirectoryName(logFile), baseFilename, savedDays);
+                        currentDate = now.Date;
                     }
                 }
                 catch
                 { }
             }
+        }
+
+        /// <summary>
+        /// Удаление наиблее старых лог-файлов свыше заданного количества
+        /// </summary>
+        /// <param name="folder">Путь к папке с лог-файлами</param>
+        /// <param name="filenamePrefix">Базовый префикс названия лог-файла</param>
+        /// <param name="savedFiles">Количество сохраняемых файлов</param>
+        /// <returns>0 - удаление успешно выполнено; иначе - удаление не выполнено</returns>
+        private static int RemoveLogFiles(string folder, string filenamePrefix, int savedFiles = 7)
+        {
+            // 1. Инициализация
+            int rc = 1;
+
+            try
+            {
+                // 2. Проверяем исходные данные
+                rc = 2;
+                if (string.IsNullOrWhiteSpace(folder) || !Directory.Exists(folder))
+                    return rc;
+
+                if (string.IsNullOrWhiteSpace(filenamePrefix))
+                    return rc;
+
+                if (savedFiles <= 0)
+                    savedFiles = 7;
+
+                // 3. Извлекаем имя файла и расширение
+                rc = 3;
+                string filename = Path.GetFileNameWithoutExtension(filenamePrefix);
+                string fileext = Path.GetExtension(filenamePrefix);
+
+                // 4. Извлекаем список файлов логов
+                rc = 4;
+                string filePattern = filename + "*";
+                if (!string.IsNullOrWhiteSpace(fileext))
+                    filePattern += fileext;
+                string[] logFiles = Directory.GetFiles(folder, filePattern);
+
+                if (logFiles == null || logFiles.Length <= savedFiles)
+                    return rc = 0;
+
+                // 5. Извлекаем время создания файлов 
+                rc = 5;
+                DateTime[] fileCreated = new DateTime[logFiles.Length];
+
+                for (int i = 0; i < logFiles.Length; i++)
+                {
+                    fileCreated[i] = System.IO.File.GetCreationTime(logFiles[i]);
+                }
+
+                // 6. Сортируем названия файлов по времени создания
+                rc = 6;
+                Array.Sort(fileCreated, logFiles);
+
+                // 7. Удаляем наиболее старые файлы
+                rc = 7;
+                int count = logFiles.Length - savedFiles;
+
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {  System.IO.File.Delete(logFiles[i]); }
+                    catch { }
+                }
+
+                // 8. Выход - Ok
+                rc = 0;
+                return rc;
+            }
+            catch
+            { return rc; }
         }
     }
 }
